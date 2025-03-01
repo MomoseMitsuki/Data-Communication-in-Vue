c8221dc79cad1232364f83a26b21917eaf3386de
MomoseMitsuki <154578028+MomoseMitsuki@users.noreply.github.com>
2025-03-01 22:46:54 +0800

    Add files via upload

---
+++ props & $emit/Vue 2/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="box">
+      <p>这里是父组件</p>
+      <div>从子组件中得到了数据为:{{ msg ? msg : '' }}</div>
+    </div>
+  <TheSon @getCount="handleCount" :count="msg"></TheSon>
+  </div>
+</template>
+
+<script>
+import TheSon from './components/TheSon.vue';
+  export default {
+    components:{
+      TheSon,
+    },
+    data(){
+      return { msg:10 }
+    },
+    methods:{
+      handleCount(val){
+        console.log(`从父组件中得到了数据为:${val}`)
+        this.msg = val
+      }
+    }
+  }
+</script>
---
+++ props & $emit/Vue 2/src/components/TheSon.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="box">
+        <p>这里是子组件</p>
+        <div>
+            <button @click="subCount">-</button>{{ count }}<button @click="addCount">+</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        count:{
+            type:Number,
+            default:10,
+            required:true,
+            validator(value){
+                console.log(`从子组件中得到了数据为:${value}`)
+                return value < 0 ? false : true
+            }
+        }
+    },
+    methods:{
+        addCount(){
+            this.$emit('getCount',this.count + 1)
+        },
+        subCount(){
+            this.$emit('getCount',this.count - 1)
+        }
+    }
+}
+</script>
+
+<style>
+.box {
+    width: 300px;
+    height: 300px;
+    border:1px solid #000;
+    padding:10px;
+    margin:20px;
+}
+</style>
