8819fdbbac0b4749c7b88d610b7d4ac4025574df
MomoseMitsuki <154578028+MomoseMitsuki@users.noreply.github.com>
2025-03-03 10:36:48 +0800

    Add files via upload

---
+++ props & $emit/Vue 3/src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+  import { ref } from 'vue'
+  import Son from '@/components/Son.vue'
+  const message = ref('')
+  const SonComponent = ref(null)
+  const sonSayHi = () => {
+    console.log(`从子组件中获得暴露的属性${SonComponent.value.count}`)
+    SonComponent.value.sayHi()
+  }
+  const changeMsg = (msg) => {
+    console.log('在父组件中获得了msg')
+    message.value = msg
+  }
+</script>
+
+<template>
+  <div class="box">
+    <h4>这里是App.vue父组件</h4>
+    <p></p>
+    <button @click="sonSayHi">通过defineExpose获取子组件属性</button>
+  </div>
+  <Son ref="SonComponent" :message="message" @send-change="changeMsg"></Son>
+</template>
+
+<style>
+  .box {
+    width: 350px;
+    height: 350px;
+    padding: 20px;
+    margin: 20px;
+    border:2px solid #000;
+    border-radius:20px;
+  }
+</style>
---
+++ props & $emit/Vue 3/src/components/Son.vue
@@ -0,0 +1,29 @@
+<script setup>
+  import { ref,defineExpose,defineEmits,defineProps } from 'vue'
+  const count = ref(10)
+  const sayHi = () => {
+    console.log('Hello, I am from The Son Component by defineExpose to you')
+  }
+  const props = defineProps({
+    message:{
+      type:String,
+      required:true
+    }
+  })
+  const emits = defineEmits(['sendChange'])
+  const sendChange = () => {
+    emits('sendChange','Hello, I am from the Father Component by defineProps to come')
+  }
+  defineExpose({
+    count,sayHi
+  })
+</script>
+
+<template>
+  <div class="box">
+    <h4>这里是Son.vue子组件</h4>
+    <button @click="count--">-</button><span> count = {{ count }} </span><button @click="count++">+</button>
+    <button @click="sendChange">在子组件触发自定义事件发送数据给父组件</button>
+    <p>从父组件通过属性接收的消息: {{ message }}</p>
+  </div>
+</template>
